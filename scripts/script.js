8ee68a5b6e7db52127863ce3b845c0cca2778257
Darya Makavchik <darjamakavchik@gmail.com>
2022-02-18 10:42:32 -0500

    выбранный уровень отображается под блоком фильтры

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,10 @@ const levelButton = document.getElementById('filterslevel');
 const statusButton = document.getElementById('filtersstatus');
 const filtersOptLevel = document.querySelector('.filters__options_type_level');
 const filtersOptStatus = document.querySelector('.filters__options_type_status');
+const selectedTemplate = document.querySelector('#selectedoption').content;
+const selectedOptions = document.querySelector('.selected');
+const selectedOption = selectedTemplate.querySelector('.selected__option').cloneNode(true);
+const checkBoxes = document.querySelectorAll('.checkbox-container__input-hidden');
 
 function changeButton (evt){
   evt.target.classList.remove('button-default_white');
@@ -21,6 +25,16 @@ function toggleFilterStatus(){
   statusButton.classList.toggle('filters-list__button_open');
 }
 
+checkBoxes.forEach(item => item.addEventListener('click', function checked(evt) {
+  if (item.checked) {
+    selectedOption.querySelector('.option__text').textContent = evt.target.parentElement.textContent;
+    selectedOptions.append(selectedOption); 
+  }
+}));
+
+
 whiteButtons.forEach(item => item.addEventListener('click', changeButton));
 levelButton.addEventListener('click', toggleFilterLevel);
 statusButton.addEventListener('click', toggleFilterStatus);
+
+
