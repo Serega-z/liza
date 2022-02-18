64e63d8a4fd3777ab76d5d45aba7bd6740e72e13
Darya Makavchik <darjamakavchik@gmail.com>
2022-02-17 21:00:57 -0500

    кнопки открывают и закрывают фильтры

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 const whiteButtons = document.querySelectorAll('.button-default_white');
+const filterButtons = document.querySelectorAll('.filter-card__button');
+const levelButton = document.getElementById('filterslevel');
+const statusButton = document.getElementById('filtersstatus');
+const filtersOptLevel = document.querySelector('.filters__options_type_level');
+const filtersOptStatus = document.querySelector('.filters__options_type_status');
 
 function changeButton (evt){
   evt.target.classList.remove('button-default_white');
@@ -6,4 +11,16 @@ function changeButton (evt){
   evt.target.innerText = "Продолжить";
 }
 
+function toggleFilterLevel(){
+  filtersOptLevel.classList.toggle('filters__options_opened');
+  levelButton.classList.toggle('filters-list__button_open');
+}
+
+function toggleFilterStatus(){
+  filtersOptStatus.classList.toggle('filters__options_opened');
+  statusButton.classList.toggle('filters-list__button_open');
+}
+
 whiteButtons.forEach(item => item.addEventListener('click', changeButton));
+levelButton.addEventListener('click', toggleFilterLevel);
+statusButton.addEventListener('click', toggleFilterStatus);
