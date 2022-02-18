0cc79fad368526d9c08ba745195b1460e7894410
Yan-Nursultan Sadykov <sadykovian@yandex.ru>
2022-02-18 21:39:43 +0300

    feat: filters chips js

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,8 @@ const statusButton = document.getElementById('filtersstatus');
 const filtersOptLevel = document.querySelector('.filters__options_type_level');
 const filtersOptStatus = document.querySelector('.filters__options_type_status');
 
+
+
 function changeButton (evt){
   evt.target.classList.remove('button-default_white');
   evt.target.classList.add('button-default_orange');
@@ -24,3 +26,76 @@ function toggleFilterStatus(){
 whiteButtons.forEach(item => item.addEventListener('click', changeButton));
 levelButton.addEventListener('click', toggleFilterLevel);
 statusButton.addEventListener('click', toggleFilterStatus);
+
+
+//filters chips
+
+const chipsContainer = document.querySelector('.chips-container');
+const checkboxContainer = document.querySelectorAll('.filters__options');
+const chipTemplate = document.querySelector('#chips-template').content
+const checkboxResetButton = document.querySelector('.filters-list__reset-button');
+
+checkedItems = {};
+
+function checkboxResetButtonActive(){
+	if(Object.entries(checkedItems).length > 0){
+		checkboxResetButton.classList.add('filters-list__reset-button_active');
+		;
+	}else{
+		checkboxResetButton.classList.remove('filters-list__reset-button_active');
+	}
+}
+
+function seachLabelText(input){
+ return input?.closest('.checkbox-container')?.textContent.trim();
+}
+
+function addChips(input, containerChips) {
+    const textInput = seachLabelText(input);
+    const chips = chipTemplate.querySelector('.chips').cloneNode(true);
+
+    chips.querySelector('.chips__text').textContent = textInput;
+    checkedItems[textInput] = {input, chips};
+
+    function handleClickChips(e) {
+        e.preventDefault();
+        checkedItems[textInput].input.checked = false;
+        delete checkedItems[textInput];
+        chips.remove();
+    }
+    chips.addEventListener('click', handleClickChips)
+
+    containerChips.append(chips)
+    return chips;
+}
+
+function removeChips(input) {
+    const textInput = seachLabelText(input);
+    const chips = checkedItems[textInput].chips;
+    delete checkedItems[textInput];
+    chips.remove();
+}
+
+function handleInputCheckbox(e) {
+    const inputCheckbox = e.target;
+
+        if (inputCheckbox.checked) {
+            addChips(inputCheckbox, chipsContainer)
+
+        } else {
+            removeChips(inputCheckbox)
+
+        }
+		checkboxResetButtonActive()
+}
+
+checkboxContainer.forEach((e)=>{
+  e.addEventListener('input', handleInputCheckbox)
+});
+
+checkboxResetButton.addEventListener('click', ()=>{
+	chipsContainer.querySelectorAll('div').forEach(el =>{
+		el.remove();
+	});
+	checkboxResetButton.classList.remove('filters-list__reset-button_active');
+})
